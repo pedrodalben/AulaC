e5741f98837fc85c7d4ba703250b501d6966e689
dalben <pedrodalbenm@outlook.com>
2017-08-21 08:43:45 -0300

    add main.c

---
+++ aula2exer1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.14
+
+int main()
+{
+    float  area,perimetro,raio;
+        printf("Digite o valor do Raio: \n");
+        scanf("%f",&raio);
+            area= PI * raio *raio;
+            perimetro= 2 * PI  * raio;
+             printf("valor da area:%6.2f\n",area );
+             printf("valor do perimetro: %6.2f\n",perimetro );
+    return 0;
+}
