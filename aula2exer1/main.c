b5c83482d18532372bbc1591d858b4b00295fbe9
pedrodalben <pedro.morais3@fatec.sp.gov.br>
2017-08-24 22:17:22 -0300

    att 24/08/2017

--- aula2exer1/main.c
+++ aula2exer1/main.c
@@ -7,8 +7,8 @@ int main()
     float  area,perimetro,raio;
         printf("Digite o valor do Raio: \n");
         scanf("%f",&raio);
-            area= PI * raio *raio;
-            perimetro= 2 * PI  * raio;
+             area= PI * raio *raio;
+             perimetro= 2 * PI  * raio;
              printf("valor da area:%6.2f\n",area );
              printf("valor do perimetro: %6.2f\n",perimetro );
     return 0;
--- aula3exer4/main.c
+++ aula3exer4/main.c
@@ -3,6 +3,29 @@
 
 int main()
 {
-    printf("Hello world!\n");
+
+    char sexo;
+    float ordenado,conta ;
+
+    printf("Digite seu sexo : F (Feminino), M (Masculino)\n");
+    sexo = getchar();
+    fflush(stdin) ;
+    printf("Digite seu salario\n");
+    scanf("%f",&ordenado);
+      switch (sexo){
+          case 'f':
+          case 'F':
+          conta = ordenado * 0.1 ;
+          break;
+          case 'm':
+          case 'M':
+          conta = ordenado * 0.15 ;
+          break;
+
+      }
+
+      printf("O o imposto pago por Voce eh de %6.2f \n " , conta);
+
+
     return 0;
 }
