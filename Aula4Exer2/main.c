732f765b887b7d25fa634bb51a988596540c7262
dalben <pedrodalbenm@outlook.com>
2017-09-14 20:40:08 -0300

    att 14/09/2017

---
+++ Aula4Exer2/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+///EXERCICIOS PAARA PRATICA///
+
+int main()
+{
+    int N , soma;
+    do{
+        printf("digite um valor entre 0 a 20 \n")
+        printf("sabendo que se voce digitar 0 ")
+    }
+    printf("Hello world!\n");
+    return 0;
+}
