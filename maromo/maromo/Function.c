f3c5509a83675eaf96605aa6a900c70198e08d9b
pedrodalben <pedro.morais3@fatec.sp.gov.br>
2017-10-15 20:54:58 -0300

    trabalho maromo

--- maromo/maromo/Function.c
+++ maromo/maromo/Function.c
@@ -1,5 +1,3 @@
-//functions
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -10,7 +8,11 @@ void  sortear(int jogo[], int t){
 
     int aux;
     int i;
-     printf(">>> Os Numeros sorteados sao: ");
+
+        printf("\t \t\t\t\t=============================\n");
+        printf("\t \t\t\t\t|           LOTO C          |\n");
+        printf("\t \t\t\t\t=============================\n\n");
+            printf(">>> Os Numeros sorteados sao: ");
 
     for(i=0; i<60; i++){
         jogo[i]=0;
@@ -22,7 +24,7 @@ void  sortear(int jogo[], int t){
    printf("%d, ",aux);
    jogo[aux] = 1;
   }
-  printf("\n");
+  printf("\n\n\n\n");
 }
 
 void palpitar(int palp[5][6], int t){
@@ -30,22 +32,27 @@ void palpitar(int palp[5][6], int t){
     int j;
     for (j=0;j<5;j++){
             system("cls");
+            system("color 02");
+            printf("\t \t\t\t\t=============================\n");
+            printf("\t \t\t\t\t|           LOTO C          |\n");
+            printf("\t \t\t\t\t=============================\n\n");
 
-    printf(" _________________________________________\n");
-    printf("|                                         |\n");
-    printf("|                 cartela %d               |\n",j+1);
-    printf("|_________________________________________|\n");
+
+            printf(" __________________________________\n");
+            printf("|                                  |\n");
+            printf("|             cartela %d            |\n",j+1);
+            printf("|__________________________________|\n");
     for(i=0; i<t; i++){
         printf("\nDigite o palpite entre 1 e 60\n %d%c = ", i+1,167);
 
         scanf("%d", &palp[j][i]);
         if (palp[j][i]>60 ){
             i=i-1;
-            printf("O Numero digitado eh maior que 60 , digite um numero entre 1 e 60\n");
+            printf("O Numero digitado eh maior que 60 \n");
         }
          if (palp[j][i]<1 ){
             i=i-1;
-            printf("O Numero digitado eh menor que 1 , digite um numero entre 1 e 60\n");
+            printf("O Numero digitado eh menor que 1 \n");
         }
     }
 }
@@ -57,36 +64,39 @@ void verificar(int jogo[],int palp[5][6], int tj, int tp){
         int i, j;
         int aux,help,k=0;
 
-        printf("Estamos Analizando os Resultados ");
+        printf("Estamos Analizando os Resultados \n");
 
         for(i=0;i<40;i++){
-        printf("> ");
+        printf(" > ");
             int c = 1, d = 1;
 
-                for ( c = 1 ; c <= 10176 ; c++ )
-                for ( d = 1 ; d <= 10276 ; d++ )
+                for ( c = 1 ; c <= 9176 ; c++ )
+                for ( d = 1 ; d <= 9276 ; d++ )
                                             {}
         }
-            printf("\n");
+            printf("\n\n\n");
          for (j=0;j<5;j++){
                 aux=0;
 
         for(i=0; i<tp; i++){
                 help=palp[j][i];
                if(jogo[help]==1){
-                        printf("Acertou o palpite : %d na cartela : %d   \n", help,j+1);
+                        printf(">>>Acertou o palpite : %d na cartela : %d   \n", help,j+1);
                         aux=aux +1;
                         k=k+1;
                 }
         }
             if (aux>1){
-                    printf("voce ganhou seu puto acertando: %d vezes na cartela : %d\n",aux,j+1);
+                    printf("\a>>>Voce ganhou seu puto acertando: %d vezes na cartela : %d\n",aux,j+1);
                 }
 
         }
          if (k==0){
 
-                    printf("\nVoce nao acertou nem um palpite\n");
+                    printf("\n>>>Voce nao acertou nem um palpite\n\n\n\n\n");
          }
+
+         printf("Trabalho Realizado Por : Ademir Pereira, Alair Bellini, Hegon Roberto, Pedro Dalben.\n ");
+         system("pause");
 
 }
